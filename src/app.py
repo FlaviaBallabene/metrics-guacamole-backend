df013f8a54c0c7acf149504b030bd27e7d0164d4
Flavia Ballabene <flavia.ballabene@hotmail.com>
2024-11-04 21:03:17 +0000

    models and login/signup routes

--- src/app.py
+++ src/app.py
@@ -36,8 +36,62 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+@app.route("/signup", methods=["POST"])
+def signup():
+    request_body = request.get_json(force=True)
+
+    required_fields = ["email", "password"]
+    for field in required_fields:
+        if field not in request_body or not request_body[field]:
+            raise APIException(f'The "{field}" field cannot be empty', 400)
+
+    verify_email = User.query.filter_by(email=request_body["email"]).first()
+    if verify_email:
+        raise APIException("An account with this email already exists", 400)
+
+    user = User(email=request_body["email"], password=request_body["password"],is_active=True)
+
+    db.session.add(user)
+
+    try:
+        db.session.commit()
+    except:
+        raise APIException('Internal error', 500)
+
+    response_body = {
+        "msg": "Successfully created user",
+        "user": user.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    request_body = request.get_json(force=True)
+    email = request_body["email"]
+    password = request_body["password"]
+    if None in [email, password]:
+        return jsonify("Some required fields are missing")
+
+    user = User.query.filter_by(email=email, password=password).first()
+    if not user:
+        return jsonify("Incorrect credentials"), 401
+
+    access_token = create_access_token(identity=user.id)
+    print(access_token)
+
+    response_body = {
+        "msg": "logged",
+        "user": user.serialize(),
+        "token": access_token
+    }
+    print(response_body),
+    return jsonify(response_body), 200
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
@@ -46,6 +100,11 @@ def handle_hello():
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
+
+
+
+# Add more routes as needed for other models like User, Project, Platform, WeeklyData
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
